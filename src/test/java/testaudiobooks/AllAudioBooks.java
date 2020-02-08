56db371f9030b115c13173691a7ce6c293825049
Maria <mstrebulyaeva@gmail.com>
2020-02-09 01:51:27 +0300

    get full list of the books have completed

--- src/test/java/testaudiobooks/AllAudioBooks.java
+++ src/test/java/testaudiobooks/AllAudioBooks.java
@@ -27,8 +27,8 @@ public class AllAudioBooks {
     By loc_loader = By.cssSelector("div.js-page-loader.page-loader");
     By loc_booklinks = By.cssSelector("a.lego-book__cover.js-image-block");
     By loc_logo = By.cssSelector("div.n-header-cell.n-header-cell--logo");
-    By loc_bottomelem = By.cssSelector("div.phone");
-    By loc_bottomelem2 = By.cssSelector("span.likely__button.likely__button_vkontakte");
+    By loc_bottomelem2 = By.cssSelector("span.p-offer__desc");
+    By loc_bottomelem = By.cssSelector("span.likely__button.likely__button_vkontakte");
 
    public void waitForPageIsLoaded (WebDriver driver) throws Exception
    {
@@ -49,78 +49,38 @@ public class AllAudioBooks {
     public List<WebElement> scrollToEnd() throws Exception {
         List<WebElement> elements = Collections.EMPTY_LIST;
         WebElement loader;
-        int size;
         Actions actions = new Actions(driver);
-        WebDriverWait longWaiter = new WebDriverWait(driver, 4);
 
         do {
-            size = elements.size();
             actions.moveToElement(driver.findElement(loc_bottomelem)).perform();
-            elements = driver.findElements(loc_booklinks);
             loader = driver.findElement(loc_loader);
             String style = driver.findElement(loc_loader).getAttribute("style");
             Log.info(">>> style:" + style+ ", isDisplayed " + loader.isDisplayed());
 
          if (loader.isDisplayed()){
-             try {
                  actions.moveToElement(driver.findElement(loc_bottomelem2)).perform();
-                 Log.info("Wait for Loader is NOT visible");
-                 longWaiter
-                         .until(ExpectedConditions.invisibilityOfElementLocated(loc_loader));
-                 Log.info("Loader is NOT visible");
-
-             } catch (Exception e) {
-                 Log.error("Loader is still visible", e);
-                 throw e;
-             }
+                 continue;
          }
          else{
              try {
-                 waiter
-                         .until(ExpectedConditions.visibilityOfElementLocated(loc_loader));
-                 Log.info("Loader have become visible. Going to wait for Loader will be invisible");
+                     waiter
+                             .until(ExpectedConditions.visibilityOfElementLocated(loc_loader));
+                     Log.info("Loader have become visible. Going to wait for Loader will be invisible");
+                     actions.moveToElement(driver.findElement(loc_bottomelem2)).perform();
+                     continue;
 
              } catch (Exception e) {
                  Log.info("Loader is already NOT visible", e);
                  break;
              }
 
-             try {
-                 longWaiter
-                         .until(ExpectedConditions.invisibilityOfElementLocated(loc_loader));
-                 Log.info("Loader is NOT visible");
-
-             } catch (Exception e) {
-                 Log.error("Loader is still visible", e);
-                 throw e;
-             }
-         }
-
-/*
-            try {
-                Log.info("Wait for loader is absent");
-                longWaiter
-                        .until(d -> {
-                            String style = d.findElement(loc_loader).getAttribute("style");
-                            Log.info(">>> style:" + style);
-                            return "display: none;".equals(style);
-                        });
-                Log.info("Loader is NOT visible");
-
-            } catch (Exception e) {
-                Log.error("Loader is still visible", e);
-                throw e;
             }
-*/
 
+        } while (true);
 
-
-            Log.info("size = " + size + " elements.size() = " + elements.size());
-        } while (size < elements.size());
-
+        elements = driver.findElements(loc_booklinks);
         Log.info("Scroll complete, elements.size() = " + elements.size());
         return elements;
-
     }
 
 
